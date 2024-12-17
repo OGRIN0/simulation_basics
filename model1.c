8095410370f4eced54f0fad13ec44767a71fae2f
AMARNATH <142997781+OGRIN0@users.noreply.github.com>
2024-12-17 16:34:15 +0530

    Update model1.c

--- model1.c
+++ model1.c
@@ -17,3 +17,38 @@ void depart(void);
 void report(void);
 void update_time_avg_stats(void);
 float expon(float mean);
+
+main() {
+    infile = fopen("mm1.in", "r");
+    outfile = fopen("mm1.out", "w");
+
+    num_events= 2;
+    fscanf(infile, "%f %f %d", &mean_interarrival, &mean_service, &num_delays_required);
+    fprintf(outfile, "single-server queueing system\n\n");
+    fprintf(outfile, "Mean interarrival time%11.3f minutes \n\n", mean_interarrival);
+    fprintf(outfile, "Mean_service time%16.3f minutes \n\n", mean_service);
+    fprint(outfile, "Number of customers%14d \n\n", num_delays_required);
+    
+    initialize();
+
+    while (num_custs_delayed < num_delays_required) {
+        timing();
+
+        update_time_avg_stats();
+
+        switch (next_event_type) {
+            case 1:
+                arrive();
+                break;
+            case 2:
+                depart();
+                break;
+                }
+                }
+    report();
+
+    fclose(infile);
+    fclose(outfile);
+
+    return 0;
+}
